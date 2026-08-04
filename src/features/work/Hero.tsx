f5302d8c0100aba63767024d931baa7e67c00bb6
Thyjano Dania <thyjano@flexsoftware.nl>
2026-08-04 13:35:00 +0200

    Update Hero component: Adjust section margin for improved layout consistency across different screen sizes.

--- src/features/work/Hero.tsx
+++ src/features/work/Hero.tsx
@@ -8,7 +8,7 @@ interface HeroProps {
 
 export default function Hero({ hero }: HeroProps) {
     return (
-        <section className="mx-10 pb-16 pt-24 lg:pb-24">
+        <section className="mx-4 lg:mx-10 pb-16 pt-24 lg:pb-24">
             <h1 className="tracking-[-5%] whitespace-pre-line">
                 { parseShortcodes(hero.work_hero_title) }
             </h1>
